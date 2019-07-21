80060f622694b23f2211dbdcf8ce5cbfd5e56e7f
923296038 <49680250+923296038@users.noreply.github.com>
2019-07-21 16:35:00 +0800

    mybatis完善

--- mybatis/src/main/java/com/jnshu/studentMapper/StudentMapper.java
+++ mybatis/src/main/java/com/jnshu/studentMapper/StudentMapper.java
@@ -7,14 +7,13 @@ import java.util.List;
 public interface StudentMapper {
 
     public Student selectUserByID(int id);
-
     public Student selectUsersByName(String name);
     public Student selectUsersById(int id);
 
-    public void addUser(Student student);
+    public int addUser(Student student);
 
-    public void updateUser(Student student);
+    public int updateUser(Student student);
 
-    public void deleteUser(int id);
+    public int deleteUser(int id);
 
 }
--- mybatis/src/test/java/TestMybatis.java
+++ mybatis/src/test/java/TestMybatis.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.io.Reader;
@@ -11,6 +13,7 @@ import java.io.Reader;
 public class TestMybatis {
     private static SqlSessionFactory sqlSessionFactory;
     private static Reader reader;
+    private static final Logger log= LogManager.getLogger(TestMybatis.class);
     static {
         try {
             reader = Resources.getResourceAsReader("Configuration.xml");
@@ -24,54 +27,62 @@ public class TestMybatis {
     }
 
     @Test
+    //增加一个用户返回id
     public void addUser() {
         Student student = new Student();
         student.setWish("place");
         student.setName("test_add");
-        student.setOnline_id(303);
+        student.setOnline_id(304);
         SqlSession session = sqlSessionFactory.openSession();
         try {
             StudentMapper userOperation = session
                     .getMapper(StudentMapper.class);
             userOperation.addUser(student);
             session.commit();
-            System.out.println("新增用户ID：" + student.getId());
+            log.info("新增用户ID：" + student.getId());
         } finally {
             session.close();
         }
     }
     @Test
+    //更改立愿
     public void updateUser() {
         SqlSession session = sqlSessionFactory.openSession();
         try {
             StudentMapper userOperation = session
                     .getMapper(StudentMapper.class);
-            Student student = userOperation.selectUserByID(29);
+            Student student = userOperation.selectUserByID(50);
             if (student != null) {
-                student.setWish("A new place");
-                userOperation.updateUser(student);
+                student.setWish("写代码");
+                int result = userOperation.updateUser(student);
                 session.commit();
+                boolean flag = result>0?true:false;
+                log.info(flag);
+            }
+            else {
+                log.info("false");
             }
         } finally {
             session.close();
         }
     }
     @Test
+    //根据id查询
     public void getUserByID() {
         SqlSession session = sqlSessionFactory.openSession();
         try {
             StudentMapper userOperation = session.getMapper(StudentMapper.class);
             //session.getMapper()的作用.已知Mapper为映射器.
-            Student student = userOperation.selectUserByID(30);
+            Student student = userOperation.selectUserByID(50);
             if (student != null) {
-                System.out.println(student.getId() + ":" + student.getName()
-                        + ":" + student.getMajor());
+                log.info(student.toString());
             }
         } finally {
             session.close();
         }
     }
     @Test
+    //根据姓名查询
     public void getUserByName(){
         SqlSession session = sqlSessionFactory.openSession();
         try {
@@ -79,21 +90,23 @@ public class TestMybatis {
             //session.getMapper()的作用.已知Mapper为映射器.
             Student student = userOperation.selectUsersByName("test_add");
             if (student != null) {
-                System.out.println(student.getId() + ":" + student.getName()
-                        + ":" + student.getMajor());
+                log.info(student.toString());
             }
         } finally {
             session.close();
         }
     }
     @Test
+    //根据id删除数据
     public void deleteUser() {
         SqlSession session = sqlSessionFactory.openSession();
         try {
             StudentMapper userOperation = session
                     .getMapper(StudentMapper.class);
-            userOperation.deleteUser(29);
+            int result = userOperation.deleteUser(50);
             session.commit();
+            boolean flag = result>0?true:false;
+            log.info(flag);
         } finally {
             session.close();
         }
